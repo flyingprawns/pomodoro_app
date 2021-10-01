312642dd5d12454bfd4a6ac792f024a09f02aef1
Qijie Zhu <qzhu9@ucsc.edu>
2021-10-01 12:20:14 -0700

    disabled multiple start button presses

--- main.py
+++ main.py
@@ -12,20 +12,38 @@ WORK_MIN = 25
 BREAK_MIN = 5
 
 # --------------------------- GLOBAL VARIABLES ---------------------------- #
+start_button_disabled = False
 taking_break = False
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    global taking_break
+    # Stop timer and reset display
+    global timer
+    window.after_cancel(timer)
     if taking_break:
-        canvas.itemconfig(timer_text, text="5:00")
+        display_time(BREAK_MIN*60)
     else:
-        canvas.itemconfig(timer_text, text="25:00")
+        display_time(WORK_MIN*60)
+    # Re-enable start button
+    global start_button_disabled
+    start_button_disabled = False
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer_button():
+    # Prevent user from pressing start button twice
+    global start_button_disabled
+    if start_button_disabled:
+        return
+    else:
+        start_button_disabled = True
+        start_timer()
+
+
 def start_timer():
+    # Start the work timer or break timer
     global taking_break
     if taking_break:
         header_label.config(text="Break!", fg=RED)
@@ -33,30 +51,34 @@ def start_timer():
     else:
         header_label.config(text="Work!", fg=GREEN)
         countdown(2)
-    reset_timer()
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def countdown(count):
-    # Display countdown
+def display_time(count):
     minutes = floor(count / 60)
     seconds = "%02d" % (count % 60)
     time_display = f"{minutes}:{seconds}"
     canvas.itemconfig(timer_text, text=time_display)
+
+
+def countdown(count):
+    display_time(count)
     # Keep counting
     if count >= 0:
-        window.after(1000, countdown, count-1)
+        global timer
+        timer = window.after(1000, countdown, count-1)
     # Countdown over!
     else:
+        # Increment pomodoro count if you were working
         global taking_break
         if not taking_break:
-            # Increment pomodoro count
             pomodoros_completed.set(pomodoros_completed.get() + 1)
         # "Flip" the break status
         taking_break = not taking_break
-        # Start timer after 3 seconds
-        window.after(3000)
+        # Start timer after 2 seconds
+        window.after(2000)
         start_timer()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Main window
@@ -71,12 +93,12 @@ canvas.create_image(119, 132, image=tomato_image)
 timer_text = canvas.create_text(119, 152, text="0:00", fill="white", font=(FONT_NAME_TIMER, 35, "bold"))
 canvas.grid(row=2, column=2)
 
-# "Timer" text
+# "Timer" label
 header_label = Label(text="Timer", font=(FONT_NAME_BUTTONS, 43, "bold"), bg=YELLOW, fg=GREEN)
 header_label.grid(row=1, column=2)
 
 # "Start" Button
-start_button = Button(text="Start", font=(FONT_NAME_BUTTONS, 15), command=start_timer)
+start_button = Button(text="Start", font=(FONT_NAME_BUTTONS, 15), command=start_timer_button)
 start_button.grid(column=1, row=3, pady=5)
 
 # "Reset" Button
