c5cf566011b159923d1f8f5b725626d91d01a0e5
Qijie Zhu <qzhu9@ucsc.edu>
2021-10-01 09:30:28 -0700

    added tomato image

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,13 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+
+tomato_image = PhotoImage(file="tomato.png")
+
+canvas = Canvas(width=240, height=264)
+canvas.create_image(120, 132, image=tomato_image)
+canvas.pack()
+
+window.mainloop()
